381c0a83317773d4f097d747824450ff2d7442ab
tannt275 <tannt275@gmail.com>
2016-02-29 23:20:02 +0700

    add Fragment Foods

---
+++ main/java/tannt275/babyfood/adapter/FoodsAdapter.java
@@ -0,0 +1,7 @@
+package tannt275.babyfood.adapter;
+
+/**
+ * Created by tannt on 2/29/2016.
+ */
+public class FoodsAdapter {
+}
---
+++ main/java/tannt275/babyfood/viewholder/FoodsViewHolder.java
@@ -0,0 +1,7 @@
+package tannt275.babyfood.viewholder;
+
+/**
+ * Created by tannt on 2/29/2016.
+ */
+public class FoodsViewHolder {
+}
