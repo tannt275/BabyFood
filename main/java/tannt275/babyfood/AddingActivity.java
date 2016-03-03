4d489f7e33f797e353b4ca5da82a7e98e03e6a4f
tannt275 <tannt275@gmail.com>
2016-03-03 11:42:23 +0700

    add AddingActivity

---
+++ main/java/tannt275/babyfood/AddingActivity.java
@@ -0,0 +1,13 @@
+package tannt275.babyfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding);
+    }
+}
