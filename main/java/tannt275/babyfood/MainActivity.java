1367a6b5366b82251b78beb7813796016204dedf
tannt275 <tannt275@gmail.com>
2016-04-22 16:56:31 +0700

    add function send feedback and rating app to MainActivity

--- main/java/tannt275/babyfood/MainActivity.java
+++ main/java/tannt275/babyfood/MainActivity.java
@@ -1,5 +1,9 @@
 package tannt275.babyfood;
 
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -12,11 +16,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 import tannt275.babyfood.common.AppUtils;
+import tannt275.babyfood.common.Log;
 import tannt275.babyfood.fragment.FavoriteFoodsFragment;
 import tannt275.babyfood.fragment.FoodInWeekFragment;
 import tannt275.babyfood.fragment.FoodsFragment;
@@ -30,7 +37,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private AdView mAdView;
     private AdRequest adRequest;
+    private DrawerLayout drawerLayout;
 
+    private int indexItemSelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,24 +51,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setItemIconTintList(null);
 
-        NutritionTowerFragment nutritionTowerFragment = new NutritionTowerFragment();
-        displayFragment(nutritionTowerFragment);
+        View headerLayout =  navigationView.getHeaderView(0);
+        headerLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout != null)
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                showDialogAction();
+            }
+        });
 
+        NutritionTowerFragment nutritionTowerFragment = new NutritionTowerFragment();
+        displayFragment(nutritionTowerFragment, getString(R.string.drawer_menu_nutrition_tower));
     }
 
-    private void displayFragment(Fragment fragment) {
+    private void displayFragment(Fragment fragment, String tagFragment) {
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setTitle(tagFragment);
+
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.mainContainer, fragment);
+        fragmentTransaction.replace(R.id.mainContainer, fragment, tagFragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
@@ -98,49 +119,120 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
         switch (id){
-            case R.id.menu_about_me:
-                showDialogAction();
-                break;
             case R.id.nav_nutrition:
                 NutritionTowerFragment nutritionTowerFragment = new NutritionTowerFragment();
-                displayFragment(nutritionTowerFragment);
+                displayFragment(nutritionTowerFragment,getString(R.string.drawer_menu_nutrition_tower));
                 break;
             case R.id.nav_food_in_week:
                 FoodInWeekFragment foodInWeekFragment = new FoodInWeekFragment();
-                displayFragment(foodInWeekFragment);
+                displayFragment(foodInWeekFragment, getString(R.string.drawer_food_in_week));
                 break;
             case R.id.nav_remember:
                 ShouldFragment shouldFragment = new ShouldFragment();
-                displayFragment(shouldFragment);
+                displayFragment(shouldFragment, getString(R.string.drawer_should));
                 break;
             case R.id.nav_waring:
                 WarningFragment warningFragment = new WarningFragment();
-                displayFragment(warningFragment);
+                displayFragment(warningFragment, getString(R.string.drawer_warning));
                 break;
             case R.id.nav_below_8_months:
                 FoodsFragment foodsFragment_below_8_months = FoodsFragment.newInstance(AppUtils.TAG_8MONTHS);
-                displayFragment(foodsFragment_below_8_months);
+                displayFragment(foodsFragment_below_8_months, getString(R.string.drawer_foods_below_8_months));
                 break;
             case R.id.nav_9_to_11_months:
                 FoodsFragment foodsFragment_9_11_months = FoodsFragment.newInstance(AppUtils.TAG_9MONTHS);
-                displayFragment(foodsFragment_9_11_months);
+                displayFragment(foodsFragment_9_11_months, getString(R.string.drawer_foods_9_11_months));
                 break;
             case R.id.nav_below_15_months:
                 FoodsFragment foodsFragment_below_15_months = FoodsFragment.newInstance(AppUtils.TAG_15MONTHS);
-                displayFragment(foodsFragment_below_15_months);
+                displayFragment(foodsFragment_below_15_months, getString(R.string.drawer_foods_15_months));
                 break;
             case R.id.nav_favorite:
                 FavoriteFoodsFragment favoriteFoodsFragment = new FavoriteFoodsFragment();
-                displayFragment(favoriteFoodsFragment);
+                displayFragment(favoriteFoodsFragment, getString(R.string.drawer_foods_favorite));
                 break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
     private void showDialogAction() {
+
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        final String [] items = new String []{"Fedback","Rating","Apps"};
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setTitle("Choose an action");
+        alertDialogBuilder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                indexItemSelected = which;
+            }
+        });
+        alertDialogBuilder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                chooseAction(indexItemSelected);
+                dialog.dismiss();
+            }
+        });
+        alertDialogBuilder.setNegativeButton("Bỏ qua", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialogBuilder.show();
+    }
+
+    private void chooseAction( int indexItemSelected) {
+        Log.e(TAG, "index of item selected: " + indexItemSelected);
+        switch (indexItemSelected){
+            case 0:
+                sendEmailToMe();
+                break;
+            case 1:
+                ratingAppsOnStore();
+                break;
+            case 2:
+                break;
+        }
+    }
+
+    private void ratingAppsOnStore() {
+
+        Uri uri = Uri.parse("market://details?id=" + MainActivity.this.getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + MainActivity.this.getPackageName())));
+        }
+    }
+
+    private void sendEmailToMe() {
+        String[] TO = {"tannt275@gmail.com"};
+        String[] CC = {"tannt.hanoi@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.e("Finished sending email...", "");
+        }
+        catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
